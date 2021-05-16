7641760f8a3257bdac8e1c09f8005619b95f9815
aa1213889 <869135421@qq.com>
2021-05-17 01:02:19 +0800

    0102

--- src/components/todoList/Content.vue
+++ src/components/todoList/Content.vue
@@ -1,9 +1,22 @@
-<template></template>
+<template>
+  <div class="todolist-content">
+    <List />
+    <List />
+    <List />
+  </div>
+</template>
 
 <script>
+import List from './List.vue'
 export default {
-  name: 'Content'
+  name: 'Content',
+  components: { List }
 }
 </script>
 
-<style></style>
+<style>
+.todolist-content {
+  width: 100%;
+  margin-top: 5%;
+}
+</style>
---
+++ src/components/todoList/List.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="todolist-list">
+    <input type="checkbox" />
+    <span>奔驰</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Content'
+}
+</script>
+
+<style>
+.todolist-list {
+  padding: 1%;
+  display: flex;
+  align-items: center;
+  border: 1px solid rgb(224, 224, 224);
+}
+.todolist-list span {
+  padding-left: 1%;
+}
+</style>
