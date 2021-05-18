d80a098d1a1ced8ef7d2d8ecf9b29e42179510d8
Zhi.Jiang <zhi.jiang@xforsec.com>
2021-05-18 10:00:58 +0800

    1000

--- src/components/todoList/Main.vue
+++ src/components/todoList/Main.vue
@@ -10,7 +10,7 @@
 import Header from './Header.vue'
 import Content from './Content.vue'
 import Footer from './Footer.vue'
-import { defineComponent, reactive, ref } from 'vue'
+import { defineComponent, reactive } from 'vue'
 import { ITodoLists } from './todos'
 
 export default defineComponent({
